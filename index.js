2c5a41e8861423362b84bfde5400a1f3449ca250
Wolfkid200444 <32025746+Wolfkid200444@users.noreply.github.com>
2018-05-11 15:52:11 -0400

    Added Changelogs
    Added Changelogs and Added a New Prefix

--- index.js
+++ index.js
@@ -9,14 +9,17 @@ client.on('ready', () => {
 client.on('message', async msg => {
     if(msg.author.bot) return;
     
-    if (msg.content === '$help') {
-    	msg.channel.send('**__Commands List__** ```[ $info | $help | $version ]```'); 
+    if (msg.content === '#help') {
+    	msg.channel.send('**__Commands List__** ```[ #info | #help | #version | #changelog]```'); 
     } else
-        if (msg.content === '@WolfKidFake help') {
+        if (msg.content === 'help') {
     	msg.channel.send('**__Commands List__** ```[ $info | $help | $version ]```');
     } else
-        if (msg.content === '$info') {
+        if (msg.content === '#info') {
     	msg.channel.send('**This bot was created on 11 May 2018 by using code with Discord.js!** [Owner: __WolfKid200444__]');
+    } else
+        if (msg.content === '#changelog') {
+    	msg.channel.send('**__Changelog__** ```___``` 
     }
 });
 
