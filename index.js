e53aa863be4146254e518a2a98b1cbe5e45af11e
Wolfkid200444 <32025746+Wolfkid200444@users.noreply.github.com>
2018-05-11 15:06:52 -0400

    Update Messages

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ client.on('message', async msg => {
     if (msg.content === '$help') {
     	msg.channel.send('**__Commands List__** ```[ $info | $help | $version ]```'); 
     } else
-        if (msg.content === 'Wolfie') {
-    	msg.channel.send('You called Wolfie. what DO YOU WANT');
+        if (msg.content === '@WolfKidFake#2042 help') {
+    	msg.channel.send('**__Commands List__** ```[ $info | $help | $version ]```');
     } else
         if (msg.content === '$info') {
     	msg.channel.send('**This bot was created on 11 May 2018 by using code with Discord.js!** [Owner: __WolfKid200444__]');
