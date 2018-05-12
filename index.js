a62649552d152d09bd196b92e0667464da23f5f1
Wolfkid200444 <32025746+Wolfkid200444@users.noreply.github.com>
2018-05-12 16:24:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     client.user.setStatus('dnd');
-    client.user.setActivity('!help**|WolfieBot!', {type: 'STREAMING'});
+    client.user.setActivity('Been Develop', {type: 'WATCHING'});
 });
 
 bot.on("message", async message => {
