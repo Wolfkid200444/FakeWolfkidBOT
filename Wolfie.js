98e433d03e01b312974af19db7b66c956ca20686
Wolfkid200444 <32025746+Wolfkid200444@users.noreply.github.com>
2018-05-13 13:13:28 -0400

    Update Wolfie.js

--- Wolfie.js
+++ Wolfie.js
@@ -13,7 +13,7 @@ client.on('message', async msg => {
     	msg.channel.send('**__Commands List__** ```[ !info | !help | !version | !changelog | !about | !channel ]```');     
     } else
         if (msg.content === '!about') {
-    	msg.channel.send('**Want to Know More About Me ?** ```________________________________``` Im a Public Bot With more Intelligent than The Normal Wolfie Im is Assistant I Help him with a lot of sfuff example tell him that bots will rule the world or etc.');
+    	msg.channel.send('**Want to Know More About Me ?** ```________________________________``` Im a Public Bot With more Intelligent than The Normal Wolfie Im is Assistant I Help him with a lot of sfuff example tell him that bots will rule the world or etc. Check out Our Github heres the link: https://github.com/Wolfkid200444/FakeWolfkidBOT And of you wanna report bugs or issue with the bot then check out our discord https://discord.gg/Z42u23M in ``#report`` or go in github in this link https://github.com/Wolfkid200444/FakeWolfkidBOT/issues Cya :3');
     } else
         if (msg.content ==="!channel") {
         msg.reply('Wanna Check out Wolfie Channel? Here is the link www.youtube.com/c/Wolfkid');
