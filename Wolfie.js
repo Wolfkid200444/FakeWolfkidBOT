80b599b205012684b90e1e1b9ec811ff89b313ed
Wolfkid200444 <32025746+Wolfkid200444@users.noreply.github.com>
2018-05-13 11:24:45 -0400

    Srach

--- Wolfie.js
+++ Wolfie.js
@@ -1,51 +1,36 @@
 const Discord = require('discord.js');
-const config = require("./config.json");
 const client = new Discord.Client();
 
 client.on('ready', () => {
     client.user.setStatus('dnd');
-    client.user.setActivity('Been Develop', {type: 'WATCHING'});
+    client.user.setActivity('!help**|WolfieBot!', {type: 'STREAMING'});
 });
 
-bot.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
-
-  let prefix = botconfig.prefix;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-
-    if(cmd === `${prefix}kick`){
-
-    //This Command is To Kick
-
-    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!kUser) return message.channel.send("Can't find user!");
-    let kReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
-    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
-
-    let kickEmbed = new Discord.RichEmbed()
-    .setDescription("~Kick~")
-    .setColor("#e56b00")
-    .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
-    .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
-    .addField("Kicked In", message.channel)
-    .addField("Tiime", message.createdAt)
-    .addField("Reason", kReason);
-
-    let kickChannel = message.guild.channels.find(`name`, "incidents");
-    if(!kickChannel) return message.channel.send("Can't find incidents channel.");
-
-    message.guild.member(kUser).kick(kReason);
-    kickChannel.send(kickEmbed);
-
-    return;
-  }
+client.on('message', async msg => {
+    if(msg.author.bot) return;
     
-    
+    if (msg.content === '!help') {
+    	msg.channel.send('**__Commands List__** ```[ !info | !help | !version | !changelog | !about ]```');     } else
+        if (msg.content === '!about') {
+    	msg.channel.send('**Want to Know More About Me ?**');
+    } else
+        if (msg.content === '!info') {
+    	msg.channel.send('**This bot was created on 11 May 2018 by using code with Discord.js!** [Owner: __WolfKid200444__] go follow him on [Twitter](https//:mobile.twitter.com/@TheRealWolfkid/)');
+    } else
+        if (msg.content === '!changelog') {
+    	msg.channel.send('**__Changelog__** ```___```');
+    } else
+        if (msg.content === '!help embed') {
+        
+        let botembed = new Discord.RichEmbed()
+        .setDescription("Help Commands")
+        .setColor("#FFFF00")
+        .setField("``!help | !changelog | !info | !about``");
+        
+        return messages.channel.send(botembed);
+    }
 });
+
 
 client.login(process.env.BOT_TOKEN);
 
