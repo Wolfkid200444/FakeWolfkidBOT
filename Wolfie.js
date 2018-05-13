925e25a61b46fa6e2ccbfb3a77be56bd69aebacd
Zade <33807566+Zadezter@users.noreply.github.com>
2018-05-13 15:27:45 +0000

    Update Wolfie.js

--- Wolfie.js
+++ Wolfie.js
@@ -10,9 +10,12 @@ client.on('message', async msg => {
     if(msg.author.bot) return;
     
     if (msg.content === '!help') {
-    	msg.channel.send('**__Commands List__** ```[ !info | !help | !version | !changelog | !about ]```');     } else
+    	msg.channel.send('**__Commands List__** ```[ !info | !help | !version | !changelog | !about ]```');     
+    } else
         if (msg.content === '!about') {
     	msg.channel.send('**Want to Know More About Me ?**');
+        if (msg.content ==="Yes") {
+        msg.reply('[Replied by !about] Okay. **Omae wa mou shindeiru!**);
     } else
         if (msg.content === '!info') {
     	msg.channel.send('**This bot was created on 11 May 2018 by using code with Discord.js!** [Owner: __WolfKid200444__] go follow him on [Twitter](https//:mobile.twitter.com/@TheRealWolfkid/)');
